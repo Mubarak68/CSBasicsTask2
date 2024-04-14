45e51858a93038b7ce8fe3203c4fcd77a9ab32c9
Mubarak Aloraifan <aloraifan505@hotmail.com>
2024-04-14 14:19:02 +0300

    done with task

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 
 //Task 3 C# Control Flow
 //Challenge 1
+Console.WriteLine("-------------<3>-------------");
 int currentHour = DateTime.Now.Hour;
 if (currentHour >= 11 && currentHour <= 14)
 {
@@ -12,13 +13,13 @@ else
     Console.WriteLine("Sorry lunch time has Finished");
 }
 
-Console.WriteLine("-------------<>-------------");
+Console.WriteLine("-------------<3.1>-------------");
 //Challenge 2
 for (int i = 1; i <= 10; i++)
 {
     Console.WriteLine(i);
 }
-Console.WriteLine("-------------<>-------------");
+Console.WriteLine("-------------<3.2>-------------");
 //Bonus
 
 int countDown = 5;
@@ -27,4 +28,21 @@ while (countDown > 0)
 {
     Console.WriteLine(countDown);
     countDown--;
+}
+
+
+Console.WriteLine("-------------<4>-------------");
+
+//C# Arrays
+
+//Challenge 1
+string[] dayOfWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+for(int i = 0; i > dayOfWeek.Length; i++)
+{
+    Console.WriteLine(dayOfWeek[i]);
+}
+//or
+foreach (var i in dayOfWeek)
+{
+    Console.WriteLine(i);
 }

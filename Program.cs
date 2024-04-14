48e957b24ed68fbd80f79b25b3094631654587c9
Mubarak Aloraifan <aloraifan505@hotmail.com>
2024-04-14 14:07:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Task 3 C# Control Flow
+//Challenge 1
+int currentHour = DateTime.Now.Hour;
+if (currentHour >= 11 && currentHour <= 14)
+{
+    Console.WriteLine("Time for Lunch!!!");
+}
+else
+{
+    Console.WriteLine("Sorry lunch time has Finished");
+}
+
+Console.WriteLine("-------------<>-------------");
+//Challenge 2
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("-------------<>-------------");
+//Bonus
+
+int countDown = 5;
+
+while (countDown > 0)
+{
+    Console.WriteLine(countDown);
+    countDown--;
+}
